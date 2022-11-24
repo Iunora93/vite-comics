8ba1b0b7a971d2db53eee5d5e25a93d4e68c2ce2
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-24 18:30:17 +0100

    Main-top img

---
+++ src/components/AppImgMain.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'AppImgMain',
+}
+</script>
+
+<template>
+
+<div>
+    <img src="../assets/buy-comics-digital-comics.png" alt="Logo">
+    <span>DIGITAL COMICS</span>
+</div>
+<div>
+    <img src="../assets/buy-comics-merchandise.png" alt="Logo">
+    <span>DC MERCHANDISE</span>
+</div>
+<div>
+    <img src="../assets/buy-comics-subscriptions.png" alt="Logo">
+    <span>SUBSCRIPTION</span>
+</div>
+<div>
+    <img src="../assets/buy-comics-shop-locator.png" alt="Logo">
+    <span>COMIC SHOP LOCATOR</span>
+</div>
+<div>
+    <img class="img-main" src="../assets/buy-dc-power-visa.svg" alt="Logo">
+    <span>DC POWER VISA</span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+    
+    div{
+        display: flex;
+        align-items: center;
+        width: calc(100% / 5);   
+    }
+    span{
+        padding: 10px;
+        color: white;
+    }
+    img{
+        width: 50px;
+    }
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,42 @@
 <script>
+import AppImgMain from './AppImgMain.vue';
 export default {
   name: 'AppMain',
+  components:{
+    AppImgMain
+  },
 }
 </script>
 
-<template>
-  <h1>Main</h1>
+<template>  
+  <main>
+    <div class="background-black">
+      <h1 class="container">Content goes here</h1>
+    </div>
+    <div class="main-top">
+      <section class="container ">
+        <AppImgMain/>
+      </section>
+    </div>
+  </main>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.background-black{
+  background-color: black;
+}
+    h1{
+      
+      color: white;
+      padding: 30px;
+    }
+    .main-top{
+      background-color: #0282f9;
+      .container{
+        display: flex;
+        justify-content: space-between;
+        padding: 30px 0 30px;
+      }
+    }
+
 </style>
