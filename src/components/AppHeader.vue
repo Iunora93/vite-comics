2e5cfc735a594eb1393591775ab028c8e32a6643
Iunora93 <eleonoraruggieri22@gmail.com>
2022-11-24 16:29:44 +0100

    Header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,61 @@
+<script>
+import AppLogo from './AppLogo.vue';
+export default {
+  name: 'AppHeader',
+  components:{
+    AppLogo
+  },
+  data() {
+    return {
+        links: [
+            'CHARACTERS',
+            'COMICS',
+            'MOVIES',
+            'TV',
+            'GAMES',
+            'COLLECTIBLES',
+            'VIDEOS',
+            'FANS',
+            'NEWS',
+            'SHOP'
+        ]
+    }
+  }
+}
+</script>
+
+<template>
+  <header class="container">
+        <AppLogo/>
+        <nav class="main-nav">
+            <ul>
+                <li v-for="link in links"><a href="">{{link}}</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    .main-nav {
+      
+        ul {
+            display: flex;
+            list-style: none;
+            
+            a {
+                display: inline-block;
+                padding: 15px 25px;
+                text-decoration: none;
+                color: #000;
+                &.active {
+                    background-color: var(--secondary-color);
+                }
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'AppLogo',
+}
+</script>
+
+<template>
+  <img src="../assets/dc-logo.png" alt="Logo">
+</template>
+
+<style>
+</style>
